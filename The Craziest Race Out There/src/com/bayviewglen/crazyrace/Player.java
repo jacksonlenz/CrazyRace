4464b86e43e3c643133906af224d8fa81e3cb2a0
JustIn-BorrowMayo <jborromeo@bayviewglen.ca>
2015-09-22 20:12:58 -0400

    Player Class Is Complete (Based on UML Specs): Changes might be needed with further programming

--- The Craziest Race Out There/src/com/bayviewglen/crazyrace/Player.java
+++ The Craziest Race Out There/src/com/bayviewglen/crazyrace/Player.java
@@ -1,5 +1,63 @@
 package com.bayviewglen.crazyrace;
+
+import java.util.ArrayList;
+
 //Player
 public class Player {
+	private String PlayerName;
+	private int PlayerPosition;
+	private int Checkpoint;
+	private int TokenNumber;
+	private ArrayList<Card> Cards;
+	
+	Player(String Name){
+		PlayerName=Name;
+		PlayerPosition=-1;
+		Checkpoint=-1;
+		TokenNumber=-1;
+	}
+	
+	public String getPlayerName() {
+		return PlayerName;
+	}
 
+	public void setPlayerName(String playerName) {
+		PlayerName = playerName;
+	}
+
+	public int getPlayerPosition() {
+		return PlayerPosition;
+	}
+	
+	public int getCheckpoint() {
+		return Checkpoint;
+	}
+
+	public void setCheckpoint(int checkpoint) {
+		Checkpoint = checkpoint;
+	}
+
+	public int getTokenNumber() {
+		return TokenNumber;
+	}
+
+	public void setTokenNumber(int tokenNumber) {
+		TokenNumber = tokenNumber;
+	}
+
+	public ArrayList<Card> getCards() {
+		return Cards;
+	}
+
+	public void adjustPosition(int move){
+		this.PlayerPosition+=move;
+	}
+	public void moveForwardOne(){
+		this.PlayerPosition++;
+	}
+	public void addToInventory(Card card){
+		this.Cards.add(card);
+	}
+	
+	
 }
